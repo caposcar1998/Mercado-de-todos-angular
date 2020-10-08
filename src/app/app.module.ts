43c96794a7168baa3c90244dbb59f324fcacea00
caposcar1998 <oscarcontrerascap@gmail.com>
2020-10-07 21:14:31 -0500

    Se implementan clases principales

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './general-components/header/header/header.component';
+import { FooterComponent } from './general-components/header/footer/footer.component';
+import { SearchBarComponent } from './index/searchBar/search-bar/search-bar.component';
+import { CarrouselComponent } from './index/carrousel/carrousel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SearchBarComponent,
+    CarrouselComponent,
   ],
   imports: [
     BrowserModule,
