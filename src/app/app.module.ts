d87df53d7fda126d3d250a640089b1c08db2b873
caposcar1998 <oscarcontrerascap@gmail.com>
2020-10-08 21:11:41 -0500

    carpetas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CarrouselComponent } from './index/carrousel/carrousel.component';
-import { SearchBarComponent } from './general-components/searchBar/search-bar/search-bar.component';
-import { FooterComponent } from './general-components/footer/footer/footer.component';
-import { HeaderComponent } from './general-components/header/header/header.component';
+import { HeaderComponent } from './general-components/header/header.component';
+import { FooterComponent } from './general-components/footer/footer.component';
+import { SearchBarComponent } from './general-components/search-bar/search-bar.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     CarrouselComponent,
-    SearchBarComponent,
-    FooterComponent,
     HeaderComponent,
+    FooterComponent,
+    SearchBarComponent,
   ],
   imports: [
     BrowserModule,
