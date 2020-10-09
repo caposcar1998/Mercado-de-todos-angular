de6a6a965754d60cba77f000ae06c4afb2681d30
caposcar1998 <oscarcontrerascap@gmail.com>
2020-10-08 21:06:23 -0500

    Se cambian de lugar carpetas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './general-components/header/header/header.component';
-import { FooterComponent } from './general-components/header/footer/footer.component';
-import { SearchBarComponent } from './index/searchBar/search-bar/search-bar.component';
 import { CarrouselComponent } from './index/carrousel/carrousel.component';
+import { SearchBarComponent } from './general-components/searchBar/search-bar/search-bar.component';
+import { FooterComponent } from './general-components/footer/footer/footer.component';
+import { HeaderComponent } from './general-components/header/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    SearchBarComponent,
     CarrouselComponent,
+    SearchBarComponent,
+    FooterComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
