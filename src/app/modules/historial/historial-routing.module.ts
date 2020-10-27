11e32cfb62b92d54d684b9d40fde7788a089f57b
Manue Ortiz Hernández <manuoh09@gmail.com>
2020-10-26 22:17:52 -0600

    Se completan componentes de Modules de Carrito, Landing, Historial

--- src/app/modules/historial/historial-routing.module.ts
+++ src/app/modules/historial/historial-routing.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HistorialComprasComponent } from './components/historial-compras/historial-compras.component';
 
+ 
 
 const routes: Routes = [
-  {path: "historial-compras", component: HistorialComprasComponent },
+  {path: "", component: HistorialComprasComponent},
 ];
 
 @NgModule({
