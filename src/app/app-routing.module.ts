3e581f127120c2be1232103f9d45d073dd83939c
Manue Ortiz Hernández <manuoh09@gmail.com>
2020-10-29 02:47:06 -0600

    Se agregaron transiciones entre routes principales

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PageNotFoundComponent } from './general-components/page-not-found/page-not-found.component';
-import { SearchBarComponent } from './general-components/search-bar/search-bar.component';
 import { CarrouselComponent } from './index/carrousel/carrousel.component';
 
 const routes: Routes = [
-  {path: "", component: CarrouselComponent},
-  {path: "login", loadChildren: "./modules/login/login.module#LoginModule"},
-
-  {path: "historial", loadChildren: "./modules/historial/historial.module#HistorialModule"},
-  {path: "perfiles", loadChildren: "./modules/perfiles/perfiles.module#PerfilesModule"},
-  {path: "producto", loadChildren: "./modules/producto/producto.module#ProductoModule"},
-  {path: "landing", loadChildren: "./modules/landing/landing.module#LandingModule"},
-  {path: "carrito", loadChildren: "./modules/carrito/carrito.module#CarritoModule"},
-  {path: "mostrar-producto", loadChildren: "./modules/mostrar-producto/mostrar-producto.module#MostrarProductoModule"},
-  {path: "**", component: PageNotFoundComponent},
+  {path: "", component: CarrouselComponent, data: {animation: "isLeft"} },
+  {path: "login", loadChildren: "./modules/login/login.module#LoginModule", data: {animation: "isLeft"}},
+  {path: "carrito", loadChildren: "./modules/carrito/carrito.module#CarritoModule",data:{animation: "isLeft"}},
+  {path: "historial", loadChildren: "./modules/historial/historial.module#HistorialModule", data:{animation: "isLeft"}},
+  {path: "perfiles", loadChildren: "./modules/perfiles/perfiles.module#PerfilesModule", data:{animation: "isLeft"}},
+  {path: "producto", loadChildren: "./modules/producto/producto.module#ProductoModule", data:{animation: "isLeft"}},
+  {path: "landing", loadChildren: "./modules/landing/landing.module#LandingModule", data:{animation: "isLeft"}},
+  
+  {path: "mostrar-producto", loadChildren: "./modules/mostrar-producto/mostrar-producto.module#MostrarProductoModule", data:{animation: "isLeft"}},
+  {path: "**", component: PageNotFoundComponent, data:{animation: "isLeft"}},
 
 ];
 
