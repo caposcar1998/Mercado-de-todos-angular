0828001384ac01ed9bc903cd093dd23c0c08406e
caposcar1998 <oscarcontrerascap@gmail.com>
2020-11-30 20:30:22 -0600

    Se añade a front

---
+++ src/app/models/producto.model.ts
@@ -0,0 +1,13 @@
+export interface Producto {
+    nombre : {type: String},
+    precio : {type: Number},
+    presentacion : {type: String},
+    costo_envio : {type : Number},
+    dias_envio : {type : Number},
+    unidades_disp : {type : Number},
+    ubicacion : {type: String},
+    fecha_exp: {type: Date},
+    descrip : {type: String},
+    img_prod : {type: String},
+}
+
