d86ec5c4070b18629392bb109c9a7c7aedbc8c23
caposcar1998 <oscarcontrerascap@gmail.com>
2020-12-01 20:31:36 -0600

    ...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,8 +18,9 @@ import { LoginButtonComponent } from './general-components/login-button/login-bu
 import { RegisterButtonComponent } from './general-components/register-button/register-button.component';
 import { LogoutButtonComponent } from './general-components/logout-button/logout-button.component';
 import { HeaderLoggedComponent } from './general-components/header-logged/header-logged.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { StorageServiceModule } from 'ngx-webstorage-service';
+import {AuthHttpInterceptor} from "@auth0/auth0-angular";
 
 @NgModule({
   declarations: [
@@ -47,7 +48,7 @@ import { StorageServiceModule } from 'ngx-webstorage-service';
         ...env.auth
     })
   ],
-  providers: [],
+  providers: [{provide: HTTP_INTERCEPTORS, useClass: AuthHttpInterceptor, multi:true},],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
