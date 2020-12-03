b47564d9bb312f3c87418ca6f9945981cd5aa577
OmarAlexDev <69879119+OmarAlexDev@users.noreply.github.com>
2020-12-03 15:26:44 -0600

    Added error catches to all controllers
    Added different error catches for the next controllers:
    -Persona
    -Catalogo
    -Producto
    -Historial
    -Carrito
    -CarritoProducto

--- backend/controllers/carritoController.ts
+++ backend/controllers/carritoController.ts
@@ -2,59 +2,95 @@ import Carrito from "../models/carrito"
 
 class CarritoController{
     getAll = async (req, res) => {
+        let statusCode =400;
         try {
           const carritos = await Carrito.find() 
           res.status(200).json(carritos)
         } catch (error) {
-            return res.status(400).json({error: error.message})
+            if(!error.statusCode){
+                error.statusCode=500;
+              }else if (error.statusCode == 400){
+                return res.status(statusCode).json({error: error.message})
+              } 
+              return res.status(error.statusCode).json({error: error.message})
         }
     }
 
     insert = async(req, res) => {
+        let statusCode = 304;
         try {
             const cart = await new Carrito (req.body).save();
             res.status(201).json(cart)
         } catch (error) {
-            
+            if(!error.statusCode){
+                error.statusCode=500;
+              }else if (error.statusCode == 304){
+                return res.status(statusCode).json({error: error.message})
+              } 
+              return res.status(error.statusCode).json({error: error.message})
         }
     }
       
     count = async (req, res) => {
+        let statusCode = 400;
         try {
           const count = await Carrito.count();
           res.status(200).json(count);
-        } catch (err) {
-          return res.status(400).json({ error: err.message });
+        } catch (error) {
+            if(!error.statusCode){
+                error.statusCode=500;
+              }else if (error.statusCode == 400){
+                return res.status(statusCode).json({error: error.message})
+              } 
+              return res.status(error.statusCode).json({error: error.message})
         }
       }
 
       // Get by id
     get = async (req, res) => {
+        let statusCode = 404;
         try {
         const obj = await Carrito.findOne({ _id: req.params.id });
         res.status(200).json(obj);
-        } catch (err) {
-        return res.status(500).json({ error: err.message });
+        } catch (error) {
+            if(!error.statusCode){
+                error.statusCode=500;
+              }else if (error.statusCode == 404){
+                return res.status(statusCode).json({error: error.message})
+              } 
+              return res.status(error.statusCode).json({error: error.message})
         }
     }
 
         // Update by id
     update = async (req, res) => {
+        let statusCode = 304;
         try {
         await Carrito.findOneAndUpdate({ _id: req.params.id }, req.body);
         res.sendStatus(200);
-        } catch (err) {
-        return res.status(400).json({ error: err.message });
+        } catch (error) {
+            if(!error.statusCode){
+                error.statusCode=500;
+              }else if (error.statusCode == 304){
+                return res.status(statusCode).json({error: error.message})
+              } 
+              return res.status(error.statusCode).json({error: error.message})
         }
     }
 
         // Delete by id
     delete = async (req, res) => {
+        let statusCode = 304;
         try {
         await Carrito.findOneAndRemove({ _id: req.params.id });
         res.sendStatus(200);
-        } catch (err) {
-        return res.status(400).json({ error: err.message });
+        } catch (error) {
+            if(!error.statusCode){
+                error.statusCode=500;
+              }else if (error.statusCode == 304){
+                return res.status(statusCode).json({error: error.message})
+              } 
+              return res.status(error.statusCode).json({error: error.message})
         }
     }
 }
--- backend/controllers/productoController.ts
+++ backend/controllers/productoController.ts
@@ -2,69 +2,111 @@ import Producto from "../models/producto"
 
 class ProductoController{
     getAll = async (req, res) => {
+        let statusCode =400;
         try {
           const productos = await Producto.find() 
           res.status(200).json(productos)
         } catch (error) {
-            return res.status(400).json({error: error.message})
+            if(!error.statusCode){
+                error.statusCode=500;
+              }else if (error.statusCode == 400){
+                return res.status(statusCode).json({error: error.message})
+              } 
+              return res.status(error.statusCode).json({error: error.message})
         }
     }
 
     insert = async(req, res) => {
+        let statusCode = 304;
         try {
             const prod = await new Producto (req.body).save();
             res.status(201).json(prod)
         } catch (error) {
-            
+            if(!error.statusCode){
+                error.statusCode=500;
+              }else if (error.statusCode == 304){
+                return res.status(statusCode).json({error: error.message})
+              } 
+              return res.status(error.statusCode).json({error: error.message})
         }
     }
 
     count = async (req, res) => {
+        let statusCode = 400;
         try {
           const count = await Producto.count();
           res.status(200).json(count);
-        } catch (err) {
-          return res.status(400).json({ error: err.message });
+        } catch (error) {
+            if(!error.statusCode){
+                error.statusCode=500;
+              }else if (error.statusCode == 400){
+                return res.status(statusCode).json({error: error.message})
+              } 
+              return res.status(error.statusCode).json({error: error.message})
         }
       }
 
       // Get by name
     getNombre = async (req, res) => {
+        let statusCode = 404;
         try {
         const obj = await Producto.find({nombre : req.query.nombre},{});
         res.status(200).json(obj);
-        } catch (err) {
-        return res.status(500).json({ error: err.message });
+        } catch (error) {
+            if(!error.statusCode){
+                error.statusCode=500;
+              }else if (error.statusCode == 404){
+                return res.status(statusCode).json({error: error.message})
+              } 
+              return res.status(error.statusCode).json({error: error.message})
         }
     }
 
       // Get by id
       get = async (req, res) => {
+        let statusCode = 404;
         try {
         const obj = await Producto.findOne({ _id: req.params.id });
         res.status(200).json(obj);
-        } catch (err) {
-        return res.status(500).json({ error: err.message });
+        } catch (error) {
+            if(!error.statusCode){
+                error.statusCode=500;
+              }else if (error.statusCode == 404){
+                return res.status(statusCode).json({error: error.message})
+              } 
+              return res.status(error.statusCode).json({error: error.message})
         }
     }
 
         // Update by id
     update = async (req, res) => {
+        let statusCode = 304;
         try {
         await Producto.findOneAndUpdate({ _id: req.params.id }, req.body);
         res.sendStatus(200);
-        } catch (err) {
-        return res.status(400).json({ error: err.message });
+        } catch (error) {
+            if(!error.statusCode){
+                error.statusCode=500;
+              }else if (error.statusCode == 304){
+                return res.status(statusCode).json({error: error.message})
+              } 
+              return res.status(error.statusCode).json({error: error.message})
         }
     }
 
         // Delete by id
     delete = async (req, res) => {
+        let statusCode = 304;
         try {
         await Producto.findOneAndRemove({ _id: req.params.id });
         res.sendStatus(200);
-        } catch (err) {
-        return res.status(400).json({ error: err.message });
+        } catch (error) {
+            if(!error.statusCode){
+                error.statusCode=500;
+              }else if (error.statusCode == 304){
+                return res.status(statusCode).json({error: error.message})
+              } 
+              return res.status(error.statusCode).json({error: error.message})
         }
     }
 }
