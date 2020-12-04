52a52d2f449043b43c55c97a051e25d9a2448579
caposcar1998 <oscarcontrerascap@gmail.com>
2020-12-03 18:40:37 -0600

    Se agrega form de editar perfil

--- src/app/modules/perfiles/components/editar-perfil-comprador/editar-perfil-comprador.component.ts
+++ src/app/modules/perfiles/components/editar-perfil-comprador/editar-perfil-comprador.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { PerfilesService } from '../../services/perfiles.service';
 import { ProfileModel, PROFILE3 } from 'src/app/models/profile.model';
+import { AuthService } from '@auth0/auth0-angular';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-editar-perfil-comprador',
@@ -10,16 +12,28 @@ import { ProfileModel, PROFILE3 } from 'src/app/models/profile.model';
 
 export class EditarPerfilCompradorComponent implements OnInit {
 
+  profileJson: string = null;
   customerProfile: ProfileModel;
+  Persona: null;
 
-  constructor(private perfilesService: PerfilesService) { }
+  constructor(private perfilesService: PerfilesService, public auth: AuthService) { }
   
   ngOnInit(): void {
-    this.perfilesService.sharedMessageCustomerProfile.subscribe(newProfile => this.customerProfile = newProfile);
+    this.auth.user$.subscribe(
+      (profile)=>(this.profileJson = JSON.stringify(profile, null, 2)));
   }
 
   updateCustomerProfile() {
-    this.perfilesService.newCustomerProfile(PROFILE3);
+    this.perfilesService.updatePersona(this.Persona,"2");
   }
+
+  updatePerfilForm = new FormGroup({
+    nombre: new FormControl(''),
+    ciudad: new FormControl(''),
+    telefono: new FormControl(''),
+    domicilio: new FormControl(''),
+  });
+
+
 
 }
